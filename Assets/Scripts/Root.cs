eaf7f68536a404977e63d84d1d6889500040b076
Tason Mathew <tasonm@amazon.com>
2019-05-01 17:29:46 +0200

    Create Root class to hold singletons in the Unity layer

---
+++ Assets/Scripts/Root.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+public class Root
+{
+    private static Dictionary<System.Type, System.Object> singletons;
+
+    public static T GetInstance<T>() where T : new()
+    {
+        if (!singletons.ContainsKey(typeof(T)))
+        {
+            singletons.Add(typeof(T), new T());
+        }
+        return (T) singletons[typeof(T)];
+    }
+}
