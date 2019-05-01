c2024fa3b6e323c84c28cc7903feb01702cb2311
Tason Mathew <tasonm@amazon.com>
2019-05-01 11:55:59 +0200

    Add basic camera controls
    Sets up a basic scene with a cube to serve as a reference point for
    camera translations.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float speed = 1;
+    private Vector3 horizontalDirectionVector = new Vector3(1, 0, 0);
+    private Vector3 verticalDirectionVector = new Vector3(0, 1, 0);
+
+    void Update()
+    {
+        handleInput();
+    }
+
+    void handleInput()
+    {
+        this.transform.position += Input.GetAxis("Horizontal") * horizontalDirectionVector * Time.deltaTime * speed;
+        this.transform.position += Input.GetAxis("Vertical") * verticalDirectionVector * Time.deltaTime * speed;
+    }
+}
