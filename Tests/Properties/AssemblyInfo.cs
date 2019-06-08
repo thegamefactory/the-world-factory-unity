86cf0f77e0409eed5a56597fc2f1bf67d37c49db
Fernand Sieber <fernand.sieber@gmail.com>
2019-06-08 15:38:19 +0200

    Tests

---
+++ Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TWF-Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TWF-Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("db523dfe-5884-4487-8b23-8022861a22ac")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tests/Tests/Input/Tools.cs
@@ -0,0 +1,35 @@
+﻿using TWF;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace TWF.Input.Tests
+{
+    [TestClass]
+    public class ToolsTest
+    {
+        private Mock<IToolApplier> toolApplier = new Mock<IToolApplier>(MockBehavior.Strict);
+        private Mock<KeyCombination> toolKeys1 = new Mock<KeyCombination>();
+        private Mock<KeyCombination> toolKeys2 = new Mock<KeyCombination>();
+        private Tool tool1 = new Tool("1", "do1", "modify1", "brush1");
+        private Tool tool2 = new Tool("2", "do2", "modify2", "brush2");
+        private Tools tools;
+
+        public void Setup()
+        {
+            tools = new Tools(() => toolApplier.Object);
+            tools.RegisterTool(toolKeys1.Object, tool1);
+            tools.RegisterTool(toolKeys2.Object, tool2);
+        }
+
+        [TestMethod]
+        public void EnactTool_NoActiveTool_DoesntDoAnything()
+        {
+            toolKeys1.Setup((k) => k.IsActive()).Returns(false);
+            toolKeys2.Setup((k) => k.IsActive()).Returns(false);
+
+            Setup();
+
+            tools.EnactTool();
+        }
+    }
+}
