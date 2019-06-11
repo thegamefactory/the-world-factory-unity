834a18efce2354e26af567bd0b137d34eba6dd50
Fernand Sieber <fernand.sieber@gmail.com>
2019-06-11 21:05:22 +0200

    Fix tool brush static initialization

--- Assets/Scripts/Core/Configuration/Defaults/ToolBrushes.cs
+++ Assets/Scripts/Core/Configuration/Defaults/ToolBrushes.cs
@@ -8,7 +8,19 @@
         public static readonly IToolBrush Manatthan = RegisterToolBrush(new ManatthanToolBrush());
         public static readonly IToolBrush Rectangle = RegisterToolBrush(new RectangleToolBrush());
 
-        public static Dictionary<string, IToolBrush> AllToolBrushes { get; } = new Dictionary<string, IToolBrush>();
+        public static Dictionary<string, IToolBrush> AllToolBrushes
+        {
+            get
+            {
+                if (null == allToolBrushes)
+                {
+                    allToolBrushes = new Dictionary<string, IToolBrush>();
+                }
+                return allToolBrushes;
+            }
+        }
+
+        private static Dictionary<string, IToolBrush> allToolBrushes;
 
         private static IToolBrush RegisterToolBrush(IToolBrush toolBrush)
         {
